8fa38a3f7a94fe7df2801238bce43f4bc3bef912
Jhonny Garcia <jhonnycgarcia@gmail.com>
2020-07-28 20:46:09 -0400

    # UP - Se añade componente footer. - Se añade componente jumbotron. - Se añade componente nav. - Se actualiza componente app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    NavComponent,
+    JumbotronComponent
   ],
   imports: [
     BrowserModule,
