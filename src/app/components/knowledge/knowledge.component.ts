7490d3ebc45bb817d21868087ef0344784cfd570
Jhonny Garcia <jhonnycgarcia@gmail.com>
2020-08-15 17:36:48 -0400

    # UP - BETA - Se añaden cambios en modo de prueba para el componente knowledge.  - Se cambia el estilo de los componentes.  - Se añaden atributos al componente knowledge para render dinamico del contenido.  - Se añaden estilos de prueba para los componentes añadidos en el HTML.

--- src/app/components/knowledge/knowledge.component.ts
+++ src/app/components/knowledge/knowledge.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KnowledgeComponent implements OnInit {
 
+  skills = [
+    {
+      title: 'Desarrollo Web',
+      fontAwesomeIcon: 'fas fa-laptop-code',
+      coverImage: 'https://res.cloudinary.com/jhonnycgarcia/image/upload/v1597463775/webdeveloper3Compress_np95rc.jpg',
+      // coverImage: 'https://res.cloudinary.com/jhonnycgarcia/image/upload/v1597463774/webDeveloper2Compress_cysrb1.jpg',
+      description: `Conocimientos basados en el desarrollo de aplicaciones web <i class="text-muted-1">(HTML – CSS- JAVASCRIPT – PHP)</i>, tanto Front-End como Back-End.`
+    },
+    {
+      title: 'Administración de Base de Datos',
+      fontAwesomeIcon: 'fas fa-database',
+      coverImage: 'https://res.cloudinary.com/jhonnycgarcia/image/upload/v1597477459/database2Compress_qnadnt.jpg',
+      description: `Manejo y administración de bases de datos relacionales <i class="text-muted-1">(Sql)</i> como no relacionales <i class="text-muted-1">(NoSql)</i> pudiendo destacar entre ellas <i class="text-muted-1">PostgreSQL, MySQL</i> y <i class="text-muted-1">MongoDB</i>.`
+    },
+    {
+      title: 'Maquetación',
+      fontAwesomeIcon: 'far fa-object-group',
+      coverImage: 'https://res.cloudinary.com/jhonnycgarcia/image/upload/v1597464326/maquetacion1Compress_dcpqrn.jpg',
+      description: `Elaborar modelos y diagramas implementando el lenguaje unificado de modelado <i class="text-muted-1">(UML)</i> para para visualizar, especificar, construir y documentar sistemas.`
+      },
+    {
+      title: 'Gestión de Proyectos',
+      fontAwesomeIcon: 'fas fa-book',
+      coverImage: 'https://res.cloudinary.com/jhonnycgarcia/image/upload/v1597499428/proyectManagement6Compress_uec2tl.jpg',
+      description: `Documentar proyectos haciendo uso de las buenas prácticas para la gestión, administración y dirección de proyectos <i class="text-muted-1">(PMBOK)</i>.`
+    }
+  ];
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
