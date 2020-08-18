151b9aed6c913042648f8b9b5565b3fee78f47bf
Jhonny Garcia <jhonnycgarcia@gmail.com>
2020-08-18 18:35:33 -0400

    # UP - Se añade libreria scroll-out al proyecto. - Se añade libreria en archivo angular.json - Se inicializa libreria dentro del componente principal.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit, OnDestroy, ElementRef } from '@angular/core';
+import ScrollOut from 'scroll-out';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit, OnDestroy {
   title = 'portafolio';
+  so: any;
+
+  constructor(private el: ElementRef){}
+
+  ngAfterContentInit(): void{
+    this.so = ScrollOut({});
+  }
+
+  ngOnDestroy(): void{
+    this.so.teardown();
+  }
 }
